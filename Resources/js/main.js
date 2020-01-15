2ca6ab2e0056b230409546d535c19c828ceb7a3f
AR Shahin <56718222+AR-Shahin@users.noreply.github.com>
2020-01-15 22:30:01 +0600

    scroll js

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -15,4 +15,21 @@ $(document).ready(function(){
     $('nav ul li').click(function(){
         $(this).addClass('active').siblings().removeClass('active');
     });
+    
+    $('.scroll').click(function(){
+        $('html,body').animate({
+            scrollTop : 0
+        }),100;
+    });
+        $('.scroll').hide();
+    $(window).scroll(function(){
+        var count = $(this).scrollTop();
+
+        if(count < 100){
+            $('.scroll').fadeOut();
+        }
+        else{
+            $('.scroll').fadeIn();
+        }
+    });
 });
